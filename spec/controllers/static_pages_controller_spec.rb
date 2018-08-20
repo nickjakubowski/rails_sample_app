0429483071c67278075fdb916795289a8bb7fa7f
Nicholas Jakubowski <ncjakubowski@gmail.com>
2018-08-19 20:15:33 -0700

    Add initial spec

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-  describe "GET home" do
+  describe "GET #home" do
+    subject { get :home}
     it "returns the correct html" do
+      expect(response).to have_http_status(:success)
+      expect(subject).to render_template(:home)
+    end
+  end
 
+  describe "GET #help" do
+    subject { get :help}
+    it "returns the correct html" do
+      expect(response).to have_http_status(:success)
+      expect(subject).to render_template(:help)
     end
   end
 
