cf2c6746ab36a3f325e635f2830b83ecbea59d4c
Nicholas Jakubowski <ncjakubowski@gmail.com>
2018-08-26 15:20:52 -0700

    Update views

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "GET #home" do
     subject { get :home}
     it "returns the correct html" do
       expect(response).to have_http_status(:success)
       expect(subject).to render_template(:home)
+      subject { should have_title("Home | #{base_title}") }
     end
   end
 
@@ -14,6 +17,7 @@ RSpec.describe StaticPagesController, type: :controller do
     it "returns the correct html" do
       expect(response).to have_http_status(:success)
       expect(subject).to render_template(:help)
+      subject { should have_title("Help | #{base_title}") }
     end
   end
 
@@ -22,6 +26,7 @@ RSpec.describe StaticPagesController, type: :controller do
     it "returns the correct html" do
       expect(response).to have_http_status(:success)
       expect(subject).to render_template(:about)
+      subject { should have_title("About | #{base_title}") }
     end
   end
 
