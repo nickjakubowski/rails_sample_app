6c5aeb4ab6c0ee4288d2bd804cfdca01fdc4c551
Nicholas Jakubowski <ncjakubowski@gmail.com>
2018-11-24 10:30:05 -0800

    Add user signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe 'POST #new' do
+    context 'new user creation' do
+      it 'errors if user info is invalid' do
+        post :create, params: { user: { name:  '',
+                                        email: 'user@invalid',
+                                        password:              'foo',
+                                        password_confirmation: 'bar' } }
+        expect(response).to have_http_status(:error)
+      end
+    end
+  end
+
 end
